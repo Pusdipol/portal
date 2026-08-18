87d4def1dc63299c3d22719c05936dc66dd6fc73
Rakagian24 <rakagian107@gmail.com>
2026-08-18 08:53:23 +0700

    feat: add post creation form component and migrate to react-quill-new

--- resources/js/components/admin/post-form.tsx
+++ resources/js/components/admin/post-form.tsx
@@ -1,6 +1,6 @@
 import { Link } from '@inertiajs/react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 import {
     Select,
     SelectContent,
